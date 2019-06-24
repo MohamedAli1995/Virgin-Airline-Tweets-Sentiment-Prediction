0786a58ee2409f64e452fa69a36789c391447378
mohamedali1995 <muhamedali166@gmail.com>
2019-06-24 23:28:47 +0200

    Updating model.

--- src/mains/main.py
+++ src/mains/main.py
@@ -6,6 +6,7 @@ from src.testers.gesture_recognition_tester import GestureRecognitionTester
 from src.utils.config import processing_config
 from src.utils.logger import Logger
 from src.utils.utils import get_args
+
 from src.utils.utils import print_predictions
 
 
@@ -13,11 +14,11 @@ def main():
     args = None
     config = None
     try:
-        args = get_args()
+        # args = get_args()
 
-        config = processing_config(args.config)
-        # config = processing_config(
-        #     "/media/syrix/programms/projects/Virgin-Airline-Tweets-Sentiment-Prediction/configs/config_model.json")
+        # config = processing_config(args.config)
+        config = processing_config(
+            "/media/syrix/programms/projects/Virgin-Airline-Tweets-Sentiment-Prediction/configs/config_model.json")
     except:
         print("Missing or invalid arguments")
         exit(0)
@@ -27,20 +28,20 @@ def main():
     model = GestureRecognitionModel(config)
     model.load(sess)
 
-    if args.input_text is not None:
-        data = DataGenerator(config, training=False)
-        data.load_test_set(args.input_text)
-        tester = GestureRecognitionTester(sess, model, data, config, logger)
-        predictions = tester.predict()
-        print_predictions(predictions)
-        return
+    # if args.input_text is not None:
+    #     data = DataGenerator(config, training=False)
+    #     data.load_test_set(args.input_text)
+    #     tester = GestureRecognitionTester(sess, model, data, config, logger)
+    #     predictions = tester.predict()
+    #     print_predictions(predictions)
+    #     return
 
 
 
     data = DataGenerator(config, training=True)
 
-    trainer = GestureRecognitionTrainer(sess, model, data, config, logger)
-    trainer.train()
+    # trainer = GestureRecognitionTrainer(sess, model, data, config, logger)
+    # trainer.train()
     tester = GestureRecognitionTester(sess, model, data, config, logger)
     tester.test()
 
